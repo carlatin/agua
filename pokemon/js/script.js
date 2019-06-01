bd2ad8520480cba08a156c86c5703f80e21bc35e
carlatin <robinsonemanuelopez@gmail.com>
2019-06-01 11:08:16 -0600

    pagina

---
+++ pokemon/js/script.js
@@ -0,0 +1,14 @@
+var pokemon= angular.module("pokemon",[])
+	pokemon.controller("listadopokemon",function($scope,$rootScope,$http){
+		$scope.Listado=[];
+		for(var i = 1; i<100;i++){
+				$http({
+				method:"GET",
+				url:"https://pokeapi.co/api/v2/pokemon/"+i
+			}).then(function success(y){
+				$scope.Listado.push(y);
+				console.log("p.data.sprites.front_default")
+			})
+		}
+	})
+	//https://pokeapi.co/api/v2/pokemon///
